1989a23b06ac0840b215575cbbba56f5da443f0a
DokaDoka <maxwelljamesmurphy@gmail.com>
2020-10-06 22:15:36 +1100

    cleanup _resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,13 +1,5 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
---resource_type 'gametype' { name = 'Hot Putsuit' }
-
-description 'Gamemode by Nynjardin.'
-
-
 server_scripts {
-    '@mysql-async/lib/MySQL.lua',
 	"server.lua"
-	}
-	 
-client_script "client.lua"
+}
